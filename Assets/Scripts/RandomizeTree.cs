5583659e42c6549892a78a12983dd4185652b7b7
Ali <alice.krouse@gmail.com>
2018-11-17 14:32:53 -0600

    manually added tree placement

---
+++ Assets/Scripts/RandomizeTree.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeTree : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    public Sprite[] trees;
+    
+	void Start ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        int randomTree = Random.Range(0, trees.Length);
+        sr.sprite = trees[randomTree];
+        Destroy(this);
+	}
+}
